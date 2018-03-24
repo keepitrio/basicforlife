842c6036e223d24c82551640dc583a8969b1a3f8
Hannie Dong <hdong92@gmail.com>
2018-03-24 12:52:56 -0700

    Create splash page. Add navigation.

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { Action } from 'react-native-router-flux';
+
+export default const Home = () => {
+  return(
+    <Text> This is home! </Text>
+    )
+}
