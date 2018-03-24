efc7e5a8adb8cea06a0afc5afac87fbca922d10b
Hannie Dong <hdong92@gmail.com>
2018-03-24 13:13:54 -0700

    Create components.

--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {Router, Scene} from 'react-native-router-flux';
 import Splash from '../Splash';
+import Home from '../Home';
+import Prescription from '../Prescription';
+import HealthRecord from '../HealthRecord';
 
 export default class Routes extends React.Component {
 
@@ -8,8 +11,11 @@ export default class Routes extends React.Component {
 
     return(
       <Router>
-        <Scene key = 'root'>
-          <Scene key = "splash" component = {Splash} title="splash" initial = {true} />
+        <Scene key='root'>
+          <Scene key="splash" component={ Splash } title="Splash" initial={true} />
+          <Scene key="home" component={ Home } title="Home" />
+          <Scene key="record" component={ HealthRecord } title="Healthrecord"  />
+          <Scene key="prescription" component={ Prescription } title="Prescription" />
         </Scene>
       </Router>
     )
