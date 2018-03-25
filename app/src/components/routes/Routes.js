c4a873e70a3bee3da3b915a648020b0df4374850
Leigh Kristine Roxas-Mobley <leighkristine@Leighs-MacBook-Pro.local>
2018-03-24 17:42:18 -0700

    added signup page, fixed routing on splash page

--- app/src/components/routes/Routes.js
+++ app/src/components/routes/Routes.js
@@ -7,6 +7,7 @@ import HealthRecord from '../HealthRecord';
 import GetTreated from '../GetTreated';
 import VideoChat from '../VideoChat';
 import ScheduleAppointment from '../ScheduleAppointment';
+import Signup from '../Signup';
 
 export default class Routes extends React.Component {
 
@@ -14,13 +15,14 @@ export default class Routes extends React.Component {
     return(
       <Router>
         <Scene key='root'>
-          <Scene key="splash" component={ Splash } />
-          <Scene key="home" component={ Home } initial={true} hideNavBar={true}/>
+          <Scene key="splash" component={ Splash } initial={true} hideNavBar={true} />
+          <Scene key="home" component={ Home } />
           <Scene key="getTreated" component={GetTreated} />
           <Scene key="videoChat" component={VideoChat} />
           <Scene key="scheduleAppointment" component={ScheduleAppointment} />
           <Scene key="record" component={ HealthRecord } />
           <Scene key="prescription" component={ Prescription } />
+          <Scene key="signup" component={ Signup } />
         </Scene>
       </Router>
     )
